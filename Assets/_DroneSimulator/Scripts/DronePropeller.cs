74d950d51f4fe4d74a244daef954933886019976
Sathish Reddy <sathish@allstargames.io>
2022-06-03 09:57:00 +0530

    Initial commit

---
+++ Assets/_DroneSimulator/Scripts/DronePropeller.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+
+namespace Deftouch
+{
+    public class DronePropeller : MonoBehaviour
+    { 
+        #region Variables
+
+        #endregion
+
+        #region UnityMethods
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region PublicMethods
+
+        #endregion
+
+        #region PrivateMethods
+
+        #endregion
+
+        #region GameEventListeners
+
+        #endregion
+    }
+}
---
+++ Assets/_DroneSimulator/Scripts/EventSystem/Events.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alpha.Events
+{
+    public enum EventId
+    {
+        EVENT_ON_PACAKAGE_PICKED_UP,
+        EVENT_ON_PACAKAGE_DELIVERED,
+    }
+}
