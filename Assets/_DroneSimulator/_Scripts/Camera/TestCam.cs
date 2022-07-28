2ddacd686c5887ff3f5caf5c9438689684383e04
Sathish Reddy <sathish@allstargames.io>
2022-07-28 16:37:10 +0530

    #Added Tools for Level Design

--- Assets/_DroneSimulator/_Scripts/Camera/TestCam.cs
+++ Assets/_DroneSimulator/_Scripts/Camera/TestCam.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Deftouch
 {
-    public class TestCam : MonoBehaviour
+    public class DroneCamera : MonoBehaviour
     {
 
         public DroneController target;
